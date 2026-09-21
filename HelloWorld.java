6bed564c85a74fb1789d82e8d2136b935b22b8a8
arwa <ar1000hd@gmail.com>
2026-09-21 23:36:22 +0300

    ^-^

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
 public static void main(String[] args) {
 System.out.println("Hello, SE4111!");
-System.out.println("Welcome to Software Construction");\
+System.out.println("Welcome to Software Construction");
   
-System.out.println("arwa");\
+System.out.println("arwa ^-^");
 
 }
 }
